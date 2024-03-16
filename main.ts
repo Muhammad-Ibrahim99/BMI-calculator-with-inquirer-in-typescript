b7016aad4faa5e9dc161388229dacfd94bdd4f1d
Muhammad Ibrahim <160318731+Muhammad-Ibrahim99@users.noreply.github.com>
2024-03-16 22:35:28 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+//BMI CALCULATOR USING INQUIRER PROMPT
+
+import inquirer from "inquirer";
+
+const questions = [
+    {type:'number', name:'weight', message:'weight(kg):'},
+    {type:'number', name:'height', message:'height(m):'},
+];
+
+const calculatorBMI = (weight: number, height: number): number => weight/(height*height);
+
+const main = async() => {
+    const {weight,height} = await inquirer.prompt(questions);
+
+    console.log(`BMI: ${calculatorBMI(weight,height).toFixed(4)}`)
+};
+
+main();
+
